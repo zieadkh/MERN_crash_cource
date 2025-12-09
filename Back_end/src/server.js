2b57112399411e60a302e56a3f2215422e192832
zieadkh <zyadk3503@gmail.com>
2025-12-09 14:28:55 +0200

    prepared for deployment

--- Back_end/src/server.js
+++ Back_end/src/server.js
@@ -4,22 +4,36 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/ratelimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT|| 5001
+const __dirname = path.resolve();
 
 
 
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
   cors({
   origin:"http://localhost:5173",
   })
 );
+}
+
 app.use(express.json());//middleware
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../Front_end/dist")));
+
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, "../Front_end", "dist", "index.html"));
+  }); 
+}
 
 
 connectDB().then(() =>{
--- Front_end/src/lib/axios.js
+++ Front_end/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL : "http://localhost:5001/api"
+  baseURL : BASE_URL,
 });
 
 export default api;
